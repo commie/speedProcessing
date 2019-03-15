1dc2828ddfe4231726a54ccf22a000ada13126dd
reirby <andreirby@gmail.com>
2019-03-15 16:36:12 -0500

    revision of heatmaps heatmaps in SVG with  humad readable axes

--- r/R_heatmap_experiment_2.0.R
+++ r/R_heatmap_experiment_2.0.R
@@ -5,7 +5,7 @@ selectedDataList <- list (duration = fullData[[4]], distance = fullData[[5]], sp
 selectedDataFrame <- as.data.frame(selectedDataList)
 
 #filteredData <- selectedDataList[which(selectedDataList$duration > 0 & is.finite(selectedDataList$speed)) & is.finite(selectedDataList$distance)),] #no neg durations, speed, no Nan no Inf
-filteredData <- selectedDataList[which(selectedDataList$duration > 0),]  #no neg durations, speed, no Nan no Inf
+filteredData <- selectedDataFrame[which(selectedDataFrame$duration > 0),]  #no neg durations, speed, no Nan no Inf
 
 # calculate log for each column
 filteredData$distance_log = log(filteredData$distance+1)
@@ -20,7 +20,7 @@ duration_log_max = max(filteredData$duration_log)
 # number of cells horizontalli and vertically
 nBins<-100
 
-# bin records into heatmap cells for speed and duration ####################################################################### 
+# bin records into heatmap cells for SPEED and DURATION ####################################################################### 
 binsx <- seq(0, duration_log_max, length=nBins)
 binsy <- seq(0, speed_log_max, length=nBins)
 
@@ -44,7 +44,7 @@ for (i in 1:numRec) {
 	}
 }
 
-# bin records into heatmap cells for speed and distance ####################################################################### 
+# bin records into heatmap cells for SPEED AND DISTANCE ####################################################################### 
 binsx <- seq(0, distance_log_max, length=nBins)
 binsy <- seq(0, speed_log_max, length=nBins)
 
@@ -68,7 +68,7 @@ for (i in 1:numRec) {
 	}
 }
 
-# bin records into heatmap cells for duration and distance ####################################################################### 
+# bin records into heatmap cells for  DURATION AND DISTANCE ####################################################################### 
 binsx <- seq(0, distance_log_max, length=nBins)
 binsy <- seq(0, duration_log_max, length=nBins)
 
@@ -124,10 +124,25 @@ lines(xfit, yfit, col = "red", lwd = 2)
 # plot heatmap #####################################################################################################################################################################
 
 
-# add zero because r needs outside borders;  break = 0.5sd central break centered on the mean;  
-# class_breaks <- c(0,0.01738625,0.88391575,1.75044525,2.61697475,3.48350425,4.35003375,5.21656325,6.08309275,6.94962225,7.81615175,8.68268125,9.54921075,10.41574025, 11.28226975) # speed-duration sd = 1.733059  mean = 5.649828 max = 11.06507
-class_breaks <- c(0,1.040024,1.834502,2.62898,3.423458,4.217936,5.012414,5.806892,6.60137,7.395848,8.190326,8.984804,9.779282) # duration-distance sd = 1.588956  mean = 5.409653 max = 9.134647
-#class_breaks <- c(0,0.3271875,1.2229525,2.1187175,3.0144825,3.9102475,4.8060125,5.7017775,6.5975425,7.4933075,8.3890725,9.2848375,10.1806025,11.0763675) # speed-distance sd = 1.79153  mean = 5.253895 max = 12.25384
+# add zero because r needs outside borders;  break = 0.5sd central break centered on the mean; 
+
+ class_breaks <- c(0,0.01738625,0.88391575,1.75044525,2.61697475,3.48350425,4.35003375,5.21656325,6.08309275,6.94962225,7.81615175,8.68268125,9.54921075,10.41574025, 11.28226975) # SPEED-DURATION sd = 1.733059  mean = 5.649828 max = 11.06507
+ x <- c(0,4.094344562,8.188689124,8.881836305,11.36674295,13.3126531,14.78173366,17.26664031); # duration 1 sec, min, hr, 2hr, day, week, month, year
+ y <- c(1.386294361, 2.302585093, 2.772588722, 3.433987204, 6.311734809); # speed  walk 3 mph, run 9 mph, bicycle  15 mph, car 30 mph, ariplane 550 mph
+ title = "SPEED-DURATION"
+
+# class_breaks <- c(0,0.3271875,1.2229525,2.1187175,3.0144825,3.9102475,4.8060125,5.7017775,6.5975425,7.4933075,8.3890725,9.2848375,10.1806025,11.0763675) # SPEED AND DISTANCE sd = 1.79153  mean = 5.253895 max = 12.25384
+# x <- c(0.000189376,0.018762282,0.055262696,0.173443934,0.693147181,2.397895273,4.615120517,6.216606101,6.908754779,8.517393171,9.21044036); #distance foot, 100 feet, 300 feet, 1000 feet, mile, 10 miles, 100 miles, 500 miles, 1000 miles, 5000 miles, 10000 miles
+# y <- c(1.386294361, 2.302585093, 2.772588722, 3.433987204, 6.311734809); # speed  walk 3 mph, run 9 mph, bicycle  15 mph, car 30 mph, ariplane 550 mph
+# title = "SPEED-DISTANCE"
+
+# class_breaks <- c(0,1.040024,1.834502,2.62898,3.423458,4.217936,5.012414,5.806892,6.60137,7.395848,8.190326,8.984804,9.779282) # duration-distance sd = 1.588956  mean = 5.409653 max = 9.134647
+# x <- c(0.000189376,0.018762282,0.055262696,0.173443934,0.693147181,2.397895273,4.615120517,6.216606101,6.908754779,8.517393171,9.21044036); #distance foot, 100 feet, 300 feet, 1000 feet, mile, 10 miles, 100 miles, 500 miles, 1000 miles, 5000 miles, 10000 miles
+# y <- c(0,4.094344562,8.188689124,8.881836305,11.36674295,13.3126531,14.78173366,17.26664031); # duration 1 sec, min, hr, 2hr, day, week, month, year
+# title = "DURATION-DISTANCE"
+
+
+
 raw_breaks <- exp(class_breaks)
 
 # set the first break to 0; make sure 1 is included into second bin
@@ -136,9 +151,12 @@ raw_breaks[2]=0.9
 
 #go to color picker for data and pick colors http://tristen.ca/hcl-picker/
 # first color is grey for background
-# class_colors <- c("#CDCDCD","#F9E8FB","#E4D5F6","#CFC3F0","#BAB2E7","#A5A1DD","#9191D1","#7D80C4","#6A71B6","#5862A6","#475395","#364582","#27376F","#192A5B")
-class_colors <- c("#CDCDCD","#F9E8FB","#E0D2F5","#C6BCEC","#ADA8E1","#9594D4","#7D80C4","#676EB3","#515C9F","#3D4A8A","#2A3A73","#192A5B")
-# class_colors <- c("#CDCDCD","#F9E8FB","#E2D4F6","#CBC0EE","#B4ADE5","#9E9BD9","#8889CC","#7378BD","#5F67AC","#4B5799","#394786","#283871","#192A5B")
+
+ class_colors <- c("#CDCDCD","#F9E8FB","#E4D5F6","#CFC3F0","#BAB2E7","#A5A1DD","#9191D1","#7D80C4","#6A71B6","#5862A6","#475395","#364582","#27376F","#192A5B") # SPEED AND DURATION
+# class_colors <- c("#CDCDCD","#F9E8FB","#E2D4F6","#CBC0EE","#B4ADE5","#9E9BD9","#8889CC","#7378BD","#5F67AC","#4B5799","#394786","#283871","#192A5B") #  SPEED AND DISTANCE
+# class_colors <- c("#CDCDCD","#F9E8FB","#E0D2F5","#C6BCEC","#ADA8E1","#9594D4","#7D80C4","#676EB3","#515C9F","#3D4A8A","#2A3A73","#192A5B")  #    DURATION AND DISTANCE
 
 #draw heatmap
 image(binsx, binsy, freq2D, zlim=c(1, max(freq2D)), col= class_colors, breaks = raw_breaks)
+title(main = title, font.main = 4)
+abline(h= y, v=x, col = "red")
