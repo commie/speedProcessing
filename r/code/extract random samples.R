c38339e8318478c7d7687bb471e6b130f0630216
reirby <andreirby@gmail.com>
2019-09-27 17:29:37 -0500

    exracted hashtags

--- r/code/extract random samples.R
+++ r/code/extract random samples.R
@@ -27,12 +27,55 @@ bottomRight_blob_DataFrame <- as.data.frame(bottomRight_blob_data_list)
 
 
 
+plane_like_hastags<-strsplit(as.character(plane_like_DataFrame$hashtag)," ")
+plane_like_hastags.freq<-table(unlist(plane_like_hastags));
+plane_like_hastags_count <- cbind.data.frame(names(plane_like_hastags.freq),as.integer(plane_like_hastags.freq))
+top100_hashtags_plane_like<-head(plane_like_hastags_count[order(-plane_like_hastags_count$`as.integer(plane_like_hastags.freq)`),],100)
+                             
+car_like_hastags<-strsplit(as.character(car_like_DataFrame$hashtag)," ")
+car_like_hastags.freq<-table(unlist(car_like_hastags));
+car_like_hastags_count <- cbind.data.frame(names(car_like_hastags.freq),as.integer(car_like_hastags.freq))
+top100_hashtags_car_like<-head(car_like_hastags_count[order(-car_like_hastags_count$`as.integer(car_like_hastags.freq)`),],100)
+                             
+top_blob_hastags<-strsplit(as.character(top_blob_DataFrame$hashtag)," ")
+top_blob_hastags.freq<-table(unlist(top_blob_hastags));
+top_blob_hastags_count <- cbind.data.frame(names(top_blob_hastags.freq),as.integer(top_blob_hastags.freq))
+top100_hashtags_top_blob<-head(top_blob_hastags_count[order(-top_blob_hastags_count$`as.integer(top_blob_hastags.freq)`),],100)
+                             
+central_blob_hastags<-strsplit(as.character(central_blob_DataFrame$hashtag)," ")
+central_blob_hastags.freq<-table(unlist(central_blob_hastags));
+central_blob_hastags_count <- cbind.data.frame(names(central_blob_hastags.freq),as.integer(central_blob_hastags.freq))
+top100_hashtags_central_blob<-head(central_blob_hastags_count[order(-central_blob_hastags_count$`as.integer(central_blob_hastags.freq)`),],100)
+                             
+edge_blob_hastags<-strsplit(as.character(edge_blob_DataFrame$hashtag)," ")
+edge_blob_hastags.freq<-table(unlist(edge_blob_hastags));
+edge_blob_hastags_count <- cbind.data.frame(names(edge_blob_hastags.freq),as.integer(edge_blob_hastags.freq))
+top100_hashtags_edge_blob<-head(edge_blob_hastags_count[order(-edge_blob_hastags_count$`as.integer(edge_blob_hastags.freq)`),],100)
+
+bottomRight_blob_hastags<-strsplit(as.character(bottomRight_blob_DataFrame$hashtag)," ")
+bottomRight_blob_hastags.freq<-table(unlist(bottomRight_blob_hastags));
+bottomRight_blob_hastags_count <- cbind.data.frame(names(bottomRight_blob_hastags.freq),as.integer(bottomRight_blob_hastags.freq))
+top100_hashtags_bottomRight_blob<-head(bottomRight_blob_hastags_count[order(-bottomRight_blob_hastags_count$`as.integer(bottomRight_blob_hastags.freq)`),],100)
+
+
+
+
+
 random_plane_like_1000 <- plane_like_DataFrame[sample(nrow(plane_like_DataFrame), 1000), ]
 random_car_like_1000 <- car_like_DataFrame[sample(nrow(car_like_DataFrame), 1000), ]
 random_top_blob_1000 <- top_blob_DataFrame[sample(nrow(top_blob_DataFrame), 1000), ]
 random_central_blob_1000 <- central_blob_DataFrame[sample(nrow(central_blob_DataFrame), 1000), ]
 random_edge_blob_1000 <- edge_blob_DataFrame[sample(nrow(edge_blob_DataFrame), 1000), ]
 random_bottomRight_blob_1000 <- bottomRight_blob_DataFrame[sample(nrow(bottomRight_blob_DataFrame), 1000), ]
+
+
+write.csv(top100_hashtags_plane_like, file = "D:/Andrei/movement_analysis/blobs/top100_hashtags_plane_like.txt")
+write.csv(top100_hashtags_car_like, file = "D:/Andrei/movement_analysis/blobs/top100_hashtags_car_like.txt")
+write.csv(top100_hashtags_top_blob, file = "D:/Andrei/movement_analysis/blobs/top100_hashtags_top_blob.txt")
+write.csv(top100_hashtags_central_blob, file = "D:/Andrei/movement_analysis/blobs/top100_hashtags_central_blob.txt")
+write.csv(top100_hashtags_edge_blob, file = "D:/Andrei/movement_analysis/blobs/top100_hashtags_edge_blob.txt")
+write.csv(top100_hashtags_bottomRight_blob, file = "D:/Andrei/movement_analysis/blobs/top100_hashtags_bottomRight_blob.txt")
+
 
 
 write.csv(random_plane_like_1000, file = "D:/Andrei/movement_analysis/blobs/random_plane_like_1000.txt")
@@ -42,3 +85,10 @@ write.csv(random_central_blob_1000, file = "D:/Andrei/movement_analysis/blobs/ra
 write.csv(random_edge_blob_1000, file = "D:/Andrei/movement_analysis/blobs/random_edge_blob_1000.txt")
 write.csv(random_bottomRight_blob_1000, file = "D:/Andrei/movement_analysis/blobs/random_bottomRight_blob_1000.txt")
 
+
+write.csv(paste(plane_like_DataFrame$text,sep="", collapse=" "),file = "D:/Andrei/movement_analysis/blobs/plane_like_DataFrame_text.txt")
+write.csv(paste(car_like_DataFrame$text,sep="", collapse=" "),file = "D:/Andrei/movement_analysis/blobs/car_like_DataFrame_text.txt")
+write.csv(paste(top_blob_DataFrame$text,sep="", collapse=" "),file = "D:/Andrei/movement_analysis/blobs/top_blob_DataFrame_text.txt")
+write.csv(paste(edge_blob_DataFrame$text,sep="", collapse=" "),file = "D:/Andrei/movement_analysis/blobs/edge_blob_DataFrame_text.txt")
+write.csv(paste(central_blob_DataFrame$text,sep="", collapse=" "),file = "D:/Andrei/movement_analysis/blobs/central_blob_DataFrame_text.txt")
+write.csv(paste(bottomRight_blob_DataFrame$text,sep="", collapse=" "),file = "D:/Andrei/movement_analysis/blobs/bottomRight_blob_DataFrame_text.txt")
